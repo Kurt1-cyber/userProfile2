ab7acb97a5686fd156bc64510da797f89d8f29c7
Kurt1-cyber <delutakurtlouie@gmail.com>
2024-11-04 21:20:21 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const userProfile = {
+  fullName: "Kurt Louie Deluta",
+  age: 19,
+  favoriteNumber: 14,
+  favoriteColors: ["red", "blue", "green", "yelllow", "black"]
+};
+
+console.log(userProfile);
